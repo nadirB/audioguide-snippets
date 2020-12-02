60d91eb318f9f206bc4d73352030bc7a89e2c270
nadir B <hecceite@nadir-B.local>
2020-12-02 13:32:04 +0100

    Initial commit

---
+++ spec/audioguide-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AudioguideSnippetsView from '../lib/audioguide-snippets-view';
+
+describe('AudioguideSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
